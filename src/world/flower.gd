26042aee6c91d57c991ae907bfac78a2b6839431
Bjørnar Prytz <bjornarprytz@gmail.com>
2025-03-07 21:04:38 +0100

    dancing flowers

--- src/world/flower.gd
+++ src/world/flower.gd
@@ -1,2 +1,16 @@
 class_name Flower
 extends Node2D
+
+@onready var pollen: CPUParticles2D = %Pollen
+@onready var animation: AnimatedSprite2D = $Animation
+
+func _ready() -> void:
+	animation.animation_finished.connect(Symphony.beat.connect.bind(_on_beat))
+	
+	pollen.modulate = Utility.random_color()
+
+func _on_beat(_number: int):
+	pollen.emitting = true
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2.ONE * 1.1, .069)
+	tween.tween_property(self, "scale", Vector2.ONE, .169)
